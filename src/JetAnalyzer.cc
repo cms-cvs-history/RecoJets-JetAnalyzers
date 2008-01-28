6bf110c8835bbf8e81ab70bd26cd60f51b3b857b
cmsbuild <>
2008-01-28 23:05:26 +0000

    This commit was manufactured by cvs2git to create tag 'RecoJets- JetAnalyzers-JetAnalyzer_17x'.
    Sprout from master 2007-10-03 22:54:16 UTC Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch> 'Example of applying 152 MCJet corrections'
    Cherrypick from master 2008-01-28 23:05:25 UTC Leonard Apanasevich <sha1-81bf5b6d8589229bad93440d7931007f605561fa@cern.ch> 'Disable PlotMCParticles and PlotLocalClusters options for now':
        interface/JetAnalyzer.h
        src/JetAnalyzer.cc
        test/JetAnalyzer.cfg

--- src/JetAnalyzer.cc
+++ src/JetAnalyzer.cc
@@ -116,6 +116,16 @@ JetAnalyzer::JetAnalyzer(const edm::ParameterSet& pSet) {
   cout << "  EtaMax: " << _EtaMax << endl;    
   cout << "  Output histograms written to: " << _HistName << std::endl;
   cout << "-------------------------------------------------------" << endl;  
+
+  if (_PlotMCParticles){
+    cout << "Sorry, PlotMCParticles option has been disabled for now" << endl;
+    _PlotMCParticles = false;
+  }
+  if (_PlotLocalClusters){
+    cout << "Sorry, PlotLocalClusters option has been disabled for now" << endl;
+    _PlotLocalClusters = false;
+  }
+
   // open the histogram file
 
   m_file=new TFile(_HistName.c_str(),"RECREATE");
@@ -190,82 +200,38 @@ void JetAnalyzer::analyze(edm::Event const& evt, edm::EventSetup const& iSetup) 
   //  cout << i << "  " << PFC[i].energy() <<endl;
   // }
 
-  try {
-    evt.getByLabel (calotowers_,caloTowers);
-  } catch (const cms::Exception& e){
-    errMsg=errMsg + "  -- No CaloTowers\n"+e.what();
-  }
-
-
-
+  evt.getByLabel (calotowers_,caloTowers);
+  if (! caloTowers.isValid() ) { errMsg=errMsg + "  -- No CaloTowers" ; caloTowers=caloTowersDummy;}
 
   if(_PlotRecHits){
-    try {
-      evt.getByLabel( "ecalRecHit","EcalRecHitsEB", EBRecHits );
-    } catch (...) {
-      errMsg=errMsg + "  -- No EB Rechits";
-    }
+    evt.getByLabel( "ecalRecHit","EcalRecHitsEB", EBRecHits );
+    evt.getByLabel( "ecalRecHit","EcalRecHitsEE", EERecHits );
+    evt.getByLabel( "hbhereco", HBHERecHits );
+    evt.getByLabel( "hfreco", HFRecHits );
+    evt.getByLabel( "horeco", HORecHits );
 
-    try {
-      evt.getByLabel( "ecalRecHit","EcalRecHitsEE", EERecHits );
-    } catch (...) {
-      errMsg=errMsg + "  -- No EE Rec hits";
-    }
-
-    try{
-      //      evt.getByType(HBHERecHits);
-      evt.getByLabel( "hbhereco", HBHERecHits );
-    } catch (...) {
-      errMsg=errMsg + "  -- No HBHE hits";
-    }
-
-    try {
-      //      evt.getByType(HFRecHits);
-      evt.getByLabel( "hfreco", HFRecHits );
-    } catch (...) {
-      errMsg=errMsg + "  -- No HF hits";
-    }
-
-    try {
-      //      evt.getByType(HORecHits);
-      evt.getByLabel( "horeco", HORecHits );
-    } catch (...) {
-      errMsg=errMsg + "  -- No HO hits";
-    }
+    if (! EBRecHits.isValid() ) { errMsg=errMsg + "  -- No EB Rechits" ; EBRecHits=EBRecHitsDummy;}
+    if (! EERecHits.isValid() ) { errMsg=errMsg + "  -- No EE Rechits" ; EERecHits=EERecHitsDummy;}
+    if (! HBHERecHits.isValid() ) { errMsg=errMsg + "  -- No HBHE Rechits" ; HBHERecHits=HBHERecHitsDummy;}
+    if (! HFRecHits.isValid() ) { errMsg=errMsg + "  -- No HF Rechits" ; HFRecHits=HFRecHitsDummy;}
+    if (! HORecHits.isValid() ) { errMsg=errMsg + "  -- No HO Rechits" ; HORecHits=HORecHitsDummy;}
   }
 
   if(_PlotDigis) {
-    try {
-      //      evt.getByType(HBHEDigis);
-      evt.getByLabel( "hcalDigis", HBHEDigis );
-    } catch (...) {
-    errMsg=errMsg + "  -- No HBHE digis";
-    }
+    evt.getByLabel( "hcalDigis", HBHEDigis );
+    evt.getByLabel( "hcalDigis", HODigis );
+    evt.getByLabel( "hcalDigis", HFDigis );
 
-
-    try {
-      evt.getByLabel( "hcalDigis", HODigis );
-      //      evt.getByType(HODigis);
-    } catch (...) {
-      errMsg=errMsg + "  -- No HO digis";
-    }
-
-    try {
-      evt.getByLabel( "hcalDigis", HFDigis );
-      //      evt.getByType(HFDigis);
-    } catch (...) {
-      errMsg=errMsg + "  -- No HF digis";
-    }
+    if (! HBHEDigis.isValid() ) { errMsg=errMsg + "  -- No HBHE digis" ; HBHEDigis=HBHEDigisDummy;}
+    if (! HODigis.isValid() ) { errMsg=errMsg + "  -- No HO digis" ; HODigis=HODigisDummy;}
+    if (! HFDigis.isValid() ) { errMsg=errMsg + "  -- No HF digis" ; HFDigis=HFDigisDummy;}
   }
 
   // Trigger Information
   
   if(_PlotTrigger){
-    try {
-      evt.getByType(trigger);
-    } catch (...) {
-      errMsg=errMsg + "  -- No TB Trigger info";
-    }
+    evt.getByType(trigger);
+    if (! trigger.isValid() ) { errMsg=errMsg + "  -- No TB Trigger info" ; trigger=triggerDummy;}
   }
 
 
@@ -523,7 +489,7 @@ void JetAnalyzer::analyze( const CaloJetCollection& calojets,
 
   // local clustering using caloTowers
 
-  if(_PlotLocalClusters) {
+  if(_PlotLocalClusters) {    
     if(&genEvent && &genJets){
       std::vector<Candidate*> ParentParton;
       GetParentPartons(ParentParton);
